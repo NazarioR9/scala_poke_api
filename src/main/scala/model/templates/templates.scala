1dbc9d5900131e1d7303aba44be8744c9c50f5ed
Muhamed Tuo <mtuo@pop-os.localdomain>
2021-05-11 12:18:29 +0200

    Add pokemon ressources

--- src/main/scala/model/templates/templates.scala
+++ src/main/scala/model/templates/templates.scala
@@ -1,50 +1,35 @@
 package model.templates
 
 case class TypeValue(name: String, url: String)
-case class Type(
-                  slot: Int,
-                  types: TypeValue
-                )
+case class Type(slot: Int, types: TypeValue)
 
-case class Stat(
-                  base_stat: Int,
-                  effort: Int,
-                  stat: TypeValue
-                )
+case class Stat(base_stat: Int, effort: Int, stat: TypeValue)
 
 case class LearnMethod(name: String, url: String)
 case class MoveLearn(level: Int, method: LearnMethod, versions: Version)
-case class Move(
-                  name: String,
-                  url: String,
-                  version_group_details: Array[MoveLearn]
-               )
+case class Move(name: String, url: String, version_group_details: Array[MoveLearn])
 
 case class AbilityInfo(name: String, url: String)
-case class Ability(
-                    is_hidden: Boolean,
-                    slot: Int,
-                    abilityInfo: AbilityInfo
-                  )
+case class Ability(is_hidden: Boolean, slot: Int, abilityInfo: AbilityInfo)
 
 case class EmptyObject()
 case class Generation(
-                       red_blue: Option[EmptyObject],
-                       yellow: Option[EmptyObject],
-                       crystal: Option[EmptyObject],
-                       gold: Option[EmptyObject],
-                       silver: Option[EmptyObject],
-                       emerald: Option[EmptyObject],
-                       firered_leafgreen: Option[EmptyObject],
-                       ruby_sapphire: Option[EmptyObject],
-                       diamond_pearl: Option[EmptyObject],
-                       heartgold_soulsilver: Option[EmptyObject],
-                       platinum: Option[EmptyObject],
-                       black_white: Option[EmptyObject],
-                       omegaruby_alphasapphire: Option[EmptyObject],
-                       x_y: Option[EmptyObject],
-                       icons: Option[EmptyObject],
-                       ultra_sun_ultra_moon: Option[EmptyObject],
+                      red_blue: Option[EmptyObject],
+                      yellow: Option[EmptyObject],
+                      crystal: Option[EmptyObject],
+                      gold: Option[EmptyObject],
+                      silver: Option[EmptyObject],
+                      emerald: Option[EmptyObject],
+                      firered_leafgreen: Option[EmptyObject],
+                      ruby_sapphire: Option[EmptyObject],
+                      diamond_pearl: Option[EmptyObject],
+                      heartgold_soulsilver: Option[EmptyObject],
+                      platinum: Option[EmptyObject],
+                      black_white: Option[EmptyObject],
+                      omegaruby_alphasapphire: Option[EmptyObject],
+                      x_y: Option[EmptyObject],
+                      icons: Option[EmptyObject],
+                      ultra_sun_ultra_moon: Option[EmptyObject],
                      )
 case class Other(dream_world: EmptyObject, official_artwork: EmptyObject)
 case class Version(
@@ -69,7 +54,6 @@ case class Sprite(
                    other: Other,
                    versions: Version
                  )
-
 case class Form(name: String, url: String)
 case class Item(name: String, url: String)
 case class Specie(name: String, url: String)
