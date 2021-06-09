7e87c1c1cd395243580ae54ad250ab59a191cfcd
Radort <radort78@gmail.com>
2021-06-09 20:42:33 +0200

    'hehe'

--- src/main/scala/application/ApplicationService.scala
+++ src/main/scala/application/ApplicationService.scala
@@ -1,8 +1,13 @@
 package application
 
+import model.out.PokeOut
+import model.templates._
+
 import java.net.URL
 import scala.io.Source
-
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import play.api.libs.json.Json.fromJson
 
 object ApplicationService {
   def process(): Unit = {
@@ -12,6 +17,19 @@ object ApplicationService {
 
     val result = Source.fromInputStream(connection.getInputStream).getLines.mkString("\n")
 
-    print(result)
+    val playJson = Json.parse(result)
+
+    println(playJson("id"))
+//    println((playJson \ "types").validate[Array[Type]](readsType).map(_.toList))
+//    val pokemon = new PokeOut(playJson("id").toString.toInt,
+//      playJson("name").toString,
+//      playJson("weight").toString.toInt,
+//      playJson("height").toString.toInt,
+//      playJson("experience").toString.toInt,
+//      playJson("types"), // Pas encore fait
+//      playJson("stats"), // Pas encore fait
+//      playJson("Moves"), // Pas encore fait
+//      playJson("Abilities") // Pas encore fait
+//      )
   }
 }
